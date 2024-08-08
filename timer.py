4d6e1ed17fb6272873d7227fbaea5e8b5272f086
Horacio H <hernandezh4@hotmail.com>
2024-08-08 00:09:52 -0700

    Continued to work on countdown, there seems to be an issue in parseTime. Will work on it tommorrow

--- timer.py
+++ timer.py
@@ -1,12 +1,44 @@
+import time
+
 def parseTime(userTime):
-    parts = userTime.split(' ')
-    parsedTime = {
-        'hours': parts[0].split(':')[0],
-        'minutes': parts[1].split(':')[1],
-        'seconds': parts[2].split(':')[2]
-    }
+    parsedTime = {}
+    if len(userTime) == 8:
+        parsedTime = {
+            'hours': userTime.split(':')[0],
+            'minutes': userTime.split(':')[1],
+            'seconds': userTime.split(':')[2]
+        }
+    elif len(userTime) == 5:
+        parsedTime = {
+            'minutes': userTime.split(':')[0],
+            'seconds': userTime.split(':')[1]
+        }
+    elif len(userTime) <= 2:
+        if(len(userTime) == 1):
+            parsedTime = {
+                'hours': '0' + userTime[0]
+            }
+        else:
+            parsedTime = {
+                'hours': userTime[0]
+            }
     return parsedTime
-#parseTime only works when input time isn't in seconds, obviously'
+
+#print(parseTime('01:01:01'))
+#print(len(parseTime('01:01:01')))
+print("If I can see this from main then python executes files when imported before they're even called")
 
 def countdown(time):
-    return print(f'Only a test to see if import works for now.')
+    parsedTime = parseTime(time)
+    timeInSeconds = int((parsedTime['hours'] * 3600) + (parsedTime['minutes'] * 60) + (parsedTime['seconds']))
+
+    while(timeInSeconds > 0 ):
+        hours = timeInSeconds // 3600
+        minutes = (timeInSeconds % 3600) // 60
+        seconds = timeInSeconds % 60
+        time_format = f'{hours:02d}:{minutes:02d}:{seconds:02d}'
+        print(time_format)
+        time.sleep(1)
+        timeInSeconds -= 1
+    if timeInSeconds == 0:
+        print(f'---Time is up---')
